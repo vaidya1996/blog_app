73f9d69299438e8aa767becc07b7f4992a7d369f
vaidyahimanshu502 <vaidyahimanshu502@gmail.com>
2024-05-27 00:13:55 +0530

    Created a factory file BlogFactory for blog table setting up factory credentials and also sets credentials inside DatabaseSedder.php file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Blog;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,6 +19,11 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            "id" => 1,
+            "password" => bcrypt("12345678"),
         ]);
+
+        //for blog data
+        Blog::factory(100)->create();
     }
 }
